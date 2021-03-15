94d79720ac0e04f531ef4cfc28de46ff0439bb25
hp <israaesraad@gmail.com>
2021-03-15 14:09:11 +0200

    version 1.0

--- app/src/main/java/com/example/samplebioapp/MainActivity.java
+++ app/src/main/java/com/example/samplebioapp/MainActivity.java
@@ -5,10 +5,26 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private EditText height;
+    private EditText weight;
+    private TextView result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void calculate(View v) {
+        String heightStr = height.getText().toString();
+        String weightStr = weight.getText().toString();
+
+        if (heightStr != null && !"".equals(heightStr)
+                && weightStr != null  &&  !"".equals(weightStr)) {
+            float heightValue = Float.parseFloat(heightStr) / 100;
+            float weightValue = Float.parseFloat(weightStr);
+
+            float bmi = weightValue / (heightValue * heightValue);
+
+            display(bmi);
+        }
     }
 }
